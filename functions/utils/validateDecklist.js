713bf205151ce9b512f2608ab480ab599355796b
Cody Bennett <hi@codyb.co>
2020-04-25 17:12:20 -0500

    Enforced deck size in validator.

--- functions/utils/validateDecklist.js
+++ functions/utils/validateDecklist.js
@@ -1,9 +1,18 @@
 //const fetch = require('node-fetch');
 //const formatSets = require('../data/formatSets');
 
+function getSize(cards) {
+  const quantities = cards.map(str => str.split(/ (.*)/)[0]);
+
+  return quantities.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+}
+
 function validateDecklist(main, side) {
   const mainboard = main.includes('\n') ? main.split('\n') : main;
   const sideboard = side.includes('\n') ? side.split('\n') : side;
+
+  if (getSize(mainboard) < 60) throw new Error('Mainboard must consist of 60 cards!');
+  if (getSize(sideboard) > 15) throw new Error('Sideboard must be fewer than 15 cards!');
 
 /*
   [mainboard, sideboard].map(async str => {
