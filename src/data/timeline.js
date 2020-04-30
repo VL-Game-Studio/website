ca98cae556d98f16ddff39548d40ede2c3e16ae2
Cody Bennett <hi@codyb.co>
2020-04-30 16:33:05 -0500

    Created timeline components.

---
+++ src/data/timeline.js
@@ -0,0 +1,147 @@
+export const formatList = [
+  'Vintage',
+  'Legacy',
+  'Commander',
+  'Pauper',
+  'Modern',
+  'Brawl',
+  'Standard',
+];
+
+export const timelineData = [
+  {
+    category: '4',
+    date: '01.2015',
+    bans: '3'
+  },
+  {
+    category: '1',
+    date: '01.2015',
+    bans: '1'
+  },
+  {
+    category: '0',
+    date: '01.2015',
+    bans: '1'
+  },
+  {
+    category: '3',
+    date: '03.2015',
+    bans: '1'
+  },
+  {
+    category: '1',
+    date: '08.2015',
+    bans: '1'
+  },
+  {
+    category: '0',
+    date: '08.2015',
+    bans: '2'
+  },
+  {
+    category: '4',
+    date: '01.2016',
+    bans: '2'
+  },
+  {
+    category: '3',
+    date: '01.2016',
+    bans: '1'
+  },
+  {
+    category: '4',
+    date: '04.2016',
+    bans: '1'
+  },
+  {
+    category: '0',
+    date: '04.2016',
+    bans: '1'
+  },
+  {
+    category: '3',
+    date: '11.2016',
+    bans: '1'
+  },
+  {
+    category: '6',
+    date: '01.2017',
+    bans: '3'
+  },
+  {
+    category: '4',
+    date: '01.2017',
+    bans: '2'
+  },
+  {
+    category: '6',
+    date: '04.2017',
+    bans: '1'
+  },
+  {
+    category: '1',
+    date: '04.2017',
+    bans: '1'
+  },
+  {
+    category: '0',
+    date: '04.2017',
+    bans: '2'
+  },
+  {
+    category: '6',
+    date: '06.2017',
+    bans: '1'
+  },
+  {
+    category: '0',
+    date: '08.2017',
+    bans: '2'
+  },
+  {
+    category: '6',
+    date: '01.2018',
+    bans: '4'
+  },
+  {
+    category: '1',
+    date: '07.2018',
+    bans: '2'
+  },
+  {
+    category: '4',
+    date: '01.2019',
+    bans: '1'
+  },
+  {
+    category: '1',
+    date: '02.2019',
+    bans: '1'
+  }
+];
+
+export const params = {
+  bgColor: 0x302D36,
+  linegridColor: 0x616061,
+  sectionColors: [0xffa280, 0xffd001, 0x6dc8df, 0x7f92ff, 0x6f6f6f, 0x2dc066, 0xd466dd, 0xFF823A, 0xFF687C, 0x8764F3, 0x78D7A7],
+  tl: {
+    numSections: 7,
+    numYearDivisions: 12,
+    numYears: 5,
+  },
+  gridElement: {
+    width: 40,
+    height: 15,
+    thickness: 4,
+    defaultColor: 0x302D36,
+  },
+  banItem: {
+    radius: 10,
+    shading: 'smooth'
+  },
+  yearBillboard: {
+    width: 30,
+    height: 15
+  }
+};
