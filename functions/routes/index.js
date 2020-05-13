335d7cf67e6ed6a82b39d9592c691a72743227d9
Cody Bennett <hi@codyb.co>
2020-05-13 02:05:56 -0500

    Removed oauth routes.

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,5 +1,4 @@
 const { Router } = require('express');
-const auth = require('./auth');
 const decklists = require('./decklists');
 const leagues = require('./leagues');
 const players = require('./players');
@@ -7,7 +6,6 @@ const results = require('./results');
 
 const router = new Router();
 
-router.use('/auth', auth);
 router.use('/decklists', decklists);
 router.use('/leagues', leagues);
 router.use('/players', players);
