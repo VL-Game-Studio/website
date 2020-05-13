7ba6cf66e167d9de416d86980389b33d0d73b46b
Cody Bennett <hi@codyb.co>
2020-05-13 00:23:54 -0500

    Enabled results endpoints.

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -3,6 +3,7 @@ const auth = require('./auth');
 const decklists = require('./decklists');
 const leagues = require('./leagues');
 const players = require('./players');
+const results = require('./results');
 
 const router = new Router();
 
@@ -10,5 +11,6 @@ router.use('/auth', auth);
 router.use('/decklists', decklists);
 router.use('/leagues', leagues);
 router.use('/players', players);
+router.use('/results', results);
 
 module.exports = router;
