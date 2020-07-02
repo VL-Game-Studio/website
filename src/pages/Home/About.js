ac77b778ef58ffa12ccd3358288a9ae3a5f12acc
Cody Bennett <hi@codyb.co>
2020-07-02 16:17:55 -0500

    Updated about page with new references.

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -3,18 +3,15 @@ import styled, { css } from 'styled-components/macro';
 import Hero from 'pages/Hero';
 import { Paragraph } from 'components/Type';
 import Anchor from 'components/Anchor';
-import Button from 'components/Button';
 
-const About = ({ alternate, ...rest }) => (
+const About = (props) => (
   <AboutHero
     label="Our Mission"
     title2="Building a stable, community Modern"
-    alternate={alternate}
-    {...rest}
+    {...props}
   >
-    <Paragraph>Ever since WAR dropped it's become increasingly clear that many Modern players have questions about the direction of current game design. Modern has stopped being a non rotating format in light of the power creep that's gone on since 2019.</Paragraph>
-    <Paragraph>Project Modern is a community-run constructed format with the card pool that was the Modern preceding WAR (<Anchor href="https://docs.google.com/document/d/13r8DzulHtOSYT7aIxY8YLkmB0NdoaZZTjgZrYAIauaY" target="_blank">see complete ban list</Anchor>) with bans centered around data-driven community feedback. Our focus is cultivating a environment with a diversity of archetypes and play patterns while maintaining balance.</Paragraph>
-    {!alternate && <Button to="/format" label="Learn More" />}
+  <Paragraph>Ever since WAR dropped it's become increasingly clear that many Modern players have questions about the direction of current game design. Modern has stopped being a non-rotating format in light of the power creep that's gone on since 2019.</Paragraph>
+  <Paragraph>Project Modern is a community-run constructed format with a card pool spanning from 8ED to ELD (<Anchor href="https://docs.google.com/document/d/1Ch4XkQkS-w3hs4UmCgQrJGHXi_hEfW3tqrR1yQQR0UA" target="_blank">see complete ban list</Anchor>) with format management centered around data-driven community feedback. Our focus is cultivating a environment with a diversity of archetypes and play patterns while maintaining format balance.</Paragraph>
   </AboutHero>
 );
 
