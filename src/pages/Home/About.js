0ab2daa91e834f899246a1cf97099aa9f33e4665
Cody Bennett <hi@codyb.co>
2020-08-04 19:57:41 -0500

    Updated about segment to match synced card pools.

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -12,8 +12,8 @@ const About = ({ alternate, ...rest }) => (
     title2="Building a stable, community Modern"
     {...rest}
   >
-    <Paragraph>Ever since WAR dropped it's become increasingly clear that many Modern players have questions about the direction of current game design. Modern has stopped being a non-rotating format in light of the power creep that's gone on since 2019.</Paragraph>
-    <Paragraph>Project Modern is a community-run constructed format with a card pool spanning from 8ED to ELD (<Anchor href="https://docs.google.com/document/d/1Ch4XkQkS-w3hs4UmCgQrJGHXi_hEfW3tqrR1yQQR0UA" target="_blank">see complete ban list</Anchor>) with format management centered around data-driven community feedback. Our focus is cultivating a environment with a diversity of archetypes and play patterns while maintaining format balance.</Paragraph>
+    <Paragraph>Ever since WAR dropped, it has become increasingly clear that many Modern players have questions about the direction of current game design. Modern has stopped being a non-rotating format in light of the power creep that's gone on since 2019.</Paragraph>
+    <Paragraph>Project Modern is a community-run constructed format (<Anchor href="https://docs.google.com/document/d/1Ch4XkQkS-w3hs4UmCgQrJGHXi_hEfW3tqrR1yQQR0UA" target="_blank">see rules & ban list</Anchor>) with format management centered around data-driven community feedback. Our focus is cultivating a environment with a diversity of archetypes and play patterns while maintaining format balance.</Paragraph>
   </Hero>
 );
 
